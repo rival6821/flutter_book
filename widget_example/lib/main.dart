d2706aa80344a8bac866208b6d69cbc7a9cf579e
ilhoon <rival6821@naver.com>
2021-12-19 21:50:09 +0900

    sum calculator

---
+++ widget_example/lib/main.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+
+void main()=> runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  static const String _title = "Widget Example";
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: _title,
+      home: WidgetApp(),
+    );
+  }
+}
+
+class WidgetApp extends StatefulWidget {
+
+  @override
+  _WidgetExampleState createState() => _WidgetExampleState();
+}
+
+class _WidgetExampleState extends State<WidgetApp> {
+  String sum = '';
+  TextEditingController value1 = TextEditingController();
+  TextEditingController value2 = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Widget Example"),
+      ),
+      body: Container(
+        child: Center(
+          child: Column(
+            children: <Widget>[
+              Padding(
+                  padding: EdgeInsets.all(15),
+                child: Text(
+                  '결과 : $sum',
+                  style: TextStyle(fontSize: 20),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(left: 20, right: 20),
+                child: TextField(keyboardType: TextInputType.number, controller: value1,),
+              ),
+              Padding(
+                padding: EdgeInsets.only(left: 20, right: 20),
+                child: TextField(keyboardType: TextInputType.number, controller: value2,),
+              ),
+              Padding(
+                padding: EdgeInsets.all(15),
+                child: ElevatedButton(child: Row(
+                  children: <Widget>[
+                    Icon(Icons.add),
+                    Text('더하기')
+                  ],
+                ), style: ButtonStyle(backgroundColor: MaterialStateProperty.all(Colors.amber))
+                ,onPressed: (){
+                  setState(() {
+                    int result = int.parse(value1.value.text) + int.parse(value2.value.text);
+                    sum = '$result';
+                  });
+                }),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
