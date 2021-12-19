8f52fdacbdf9ae16f9e8db2f7c646a36df30767f
ilhoon <rival6821@naver.com>
2021-12-19 20:20:54 +0900

    init

---
+++ flutter_app_test/lib/imageWidget.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ImageWidgetApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState(){
+    return _ImageWidgetApp();
+  }
+}
+
+class _ImageWidgetApp extends State<ImageWidgetApp> {
+  @override
+  Widget build(BuildContext context) {
+    return null;
+  }
+}
