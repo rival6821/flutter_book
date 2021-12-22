226c9594d2254acbaa1072caca047c3193458fc5
ilhoon <rival6821@naver.com>
2021-12-22 21:47:23 +0900

    dropdown

--- widget_example/lib/main.dart
+++ widget_example/lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-void main()=> runApp(MyApp());
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   static const String _title = "Widget Example";
@@ -15,15 +15,31 @@ class MyApp extends StatelessWidget {
 }
 
 class WidgetApp extends StatefulWidget {
-
   @override
   _WidgetExampleState createState() => _WidgetExampleState();
 }
 
 class _WidgetExampleState extends State<WidgetApp> {
-  String sum = '';
+  List _buttonList = ['더하기', '빼기', '곱하기', '나누기'];
+  List<DropdownMenuItem<String>> _dropDownMenuItems =
+      new List.empty(growable: true);
+  String? _buttonText;
+
+  String calResult = '';
   TextEditingController value1 = TextEditingController();
   TextEditingController value2 = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    for (var item in _buttonList) {
+      _dropDownMenuItems.add(DropdownMenuItem(
+        child: Text(item),
+        value: item,
+      ));
+    }
+    _buttonText = _dropDownMenuItems[0].value;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -36,34 +52,64 @@ class _WidgetExampleState extends State<WidgetApp> {
           child: Column(
             children: <Widget>[
               Padding(
-                  padding: EdgeInsets.all(15),
+                padding: EdgeInsets.all(15),
+                child: DropdownButton(
+                  items: _dropDownMenuItems,
+                  onChanged: (String? value) {
+                    setState(() {
+                      _buttonText = value;
+                    });
+                  },
+                  value: _buttonText,
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.all(15),
                 child: Text(
-                  '결과 : $sum',
+                  '결과 : $calResult',
                   style: TextStyle(fontSize: 20),
                 ),
               ),
               Padding(
                 padding: EdgeInsets.only(left: 20, right: 20),
-                child: TextField(keyboardType: TextInputType.number, controller: value1,),
+                child: TextField(
+                  keyboardType: TextInputType.number,
+                  controller: value1,
+                ),
               ),
               Padding(
                 padding: EdgeInsets.only(left: 20, right: 20),
-                child: TextField(keyboardType: TextInputType.number, controller: value2,),
+                child: TextField(
+                  keyboardType: TextInputType.number,
+                  controller: value2,
+                ),
               ),
               Padding(
                 padding: EdgeInsets.all(15),
-                child: ElevatedButton(child: Row(
-                  children: <Widget>[
-                    Icon(Icons.add),
-                    Text('더하기')
-                  ],
-                ), style: ButtonStyle(backgroundColor: MaterialStateProperty.all(Colors.amber))
-                ,onPressed: (){
-                  setState(() {
-                    int result = int.parse(value1.value.text) + int.parse(value2.value.text);
-                    sum = '$result';
-                  });
-                }),
+                child: ElevatedButton(
+                    child: Row(
+                      children: <Widget>[Icon(Icons.add), Text(_buttonText!)],
+                    ),
+                    style: ButtonStyle(
+                        backgroundColor:
+                            MaterialStateProperty.all(Colors.amber)),
+                    onPressed: () {
+                      setState(() {
+                        var value1Int = double.parse(value1.value.text);
+                        var value2Int = double.parse(value2.value.text);
+                        var result;
+                        if (_buttonText == '더하기') {
+                          result = value1Int + value2Int;
+                        } else if (_buttonText == '빼기') {
+                          result = value1Int - value2Int;
+                        } else if (_buttonText == '곱하기') {
+                          result = value1Int * value2Int;
+                        } else {
+                          result = value1Int / value2Int;
+                        }
+                        calResult = '$result';
+                      });
+                    }),
               ),
             ],
           ),
