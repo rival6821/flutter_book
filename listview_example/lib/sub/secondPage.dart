8895828afbbc44e71b73c4817bedffa4de8c8801
ilhoon <rival6821@naver.com>
2021-12-26 20:20:43 +0900

    tabbar list

--- listview_example/lib/sub/secondPage.dart
+++ listview_example/lib/sub/secondPage.dart
@@ -1,18 +1,187 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import '../animalItem.dart';
 
-class SecondApp extends StatelessWidget {
+class SecondApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _SecondApp();
+
   final List<Animal>? list;
+
   SecondApp({Key? key, this.list}) : super(key: key);
+}
+
+class _SecondApp extends State<SecondApp> {
+  final nameController = TextEditingController();
+  int? _radioValue = 0;
+  bool? flyExist = false;
+  String? _imagePath;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
         child: Center(
-          child: Text('두번째 페이지'),
-        ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                TextField(
+                  controller: nameController,
+                  keyboardType: TextInputType.text,
+                  maxLength: 10,
+                ),
+                Row(
+                  children: <Widget>[
+                    Radio(
+                        value: 0,
+                        groupValue: _radioValue,
+                        onChanged: _radioChange),
+                    Text('양서류'),
+                    Radio(
+                        value: 1,
+                        groupValue: _radioValue,
+                        onChanged: _radioChange),
+                    Text('파충류'),
+                    Radio(
+                        value: 2,
+                        groupValue: _radioValue,
+                        onChanged: _radioChange),
+                    Text('포유류'),
+                  ],
+                ),
+                Row(
+                  children: <Widget>[
+                    Text('날 수 있나요?'),
+                    Checkbox(
+                        value: flyExist,
+                        onChanged: (bool? check) {
+                          setState(() {
+                            flyExist = check;
+                          });
+                        })
+                  ],
+                ),
+                SizedBox(
+                  height: 100,
+                  child: ListView(
+                    scrollDirection: Axis.horizontal,
+                    children: <Widget>[
+                      GestureDetector(
+                        child: Image.asset(
+                          'repo/images/cow.png',
+                          width: 80,
+                        ),
+                        onTap: () {
+                          _imagePath = 'repo/images/cow.png';
+                        },
+                      ),
+                      GestureDetector(
+                        child: Image.asset(
+                          'repo/images/pig.png',
+                          width: 80,
+                        ),
+                        onTap: () {
+                          _imagePath = 'repo/images/pig.png';
+                        },
+                      ),
+                      GestureDetector(
+                        child: Image.asset(
+                          'repo/images/bee.png',
+                          width: 80,
+                        ),
+                        onTap: () {
+                          _imagePath = 'repo/images/bee.png';
+                        },
+                      ),
+                      GestureDetector(
+                        child: Image.asset(
+                          'repo/images/cat.png',
+                          width: 80,
+                        ),
+                        onTap: () {
+                          _imagePath = 'repo/images/cat.png';
+                        },
+                      ),
+                      GestureDetector(
+                        child: Image.asset(
+                          'repo/images/dog.png',
+                          width: 80,
+                        ),
+                        onTap: () {
+                          _imagePath = 'repo/images/dog.png';
+                        },
+                      ),
+                      GestureDetector(
+                        child: Image.asset(
+                          'repo/images/fox.png',
+                          width: 80,
+                        ),
+                        onTap: () {
+                          _imagePath = 'repo/images/fox.png';
+                        },
+                      ),
+                      GestureDetector(
+                        child: Image.asset(
+                          'repo/images/monkey.png',
+                          width: 80,
+                        ),
+                        onTap: () {
+                          _imagePath = 'repo/images/monkey.png';
+                        },
+                      ),
+                    ],
+                  ),
+                ),
+                ElevatedButton(
+                    onPressed: () {
+                      var animal = Animal(
+                          animalName: nameController.value.text,
+                          kind: getKind(_radioValue),
+                          imagePath: _imagePath,
+                          flyExist: flyExist);
+                      AlertDialog dialog = AlertDialog(
+                        title: const Text('동물 추가하기'),
+                        content: Text(
+                          '이 동물은 ${animal.animalName} 입니다.'
+                              '또 동물의 종류는 ${animal.kind}입니다. \n이 동물을 추가하시겠습니까?',
+                          style: const TextStyle(fontSize: 30.0),
+                        ),
+                        actions: [
+                          ElevatedButton(onPressed: (){
+                            widget.list?.add(animal);
+                            log('yes', error: animal);
+                            Navigator.of(context).pop();
+                          }, child: const Text('예')),
+                          ElevatedButton(onPressed: (){
+                            Navigator.of(context).pop();
+                          }, child: const Text('아니요')),
+                        ],
+                      );
+                      showDialog(context: context, builder: (BuildContext context) => dialog);
+                    },
+                    child: Text('동물 추가하기')
+                )
+              ],
+            )),
       ),
     );
+  }
+
+  _radioChange(int? value) {
+    setState(() {
+      _radioValue = value;
+    });
+  }
+
+  getKind(int? radioValue) {
+    switch (radioValue) {
+      case 0:
+        return '양서류';
+      case 1:
+        return '파충류';
+      case 2:
+        return '포유류';
+    }
   }
 }
