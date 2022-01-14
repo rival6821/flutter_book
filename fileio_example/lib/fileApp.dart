90501614cfb7bd4618fb79bb09792dcb7aeba7f4
ilhoon <rival6821@naver.com>
2022-01-14 23:27:48 +0900

    file io

---
+++ fileio_example/lib/fileApp.dart
@@ -0,0 +1,62 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
+
+class FileApp extends StatefulWidget {
+  @override
+  _FileApp createState() => _FileApp();
+}
+
+class _FileApp extends State<FileApp> {
+  int _count = 0;
+  @override
+  void initState() {
+    super.initState();
+    readCountFile();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('File Example'),
+      ),
+      body: Container(
+        child: Center(
+          child: Text(
+            '$_count',
+            style: TextStyle(fontSize: 40),
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            _count++;
+          });
+          writeCountFile(_count);
+        },
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+
+  void writeCountFile(int count) async {
+    var dir = await getApplicationDocumentsDirectory();
+    File(dir.path + '/count.txt').writeAsStringSync(count.toString());
+  }
+
+  void readCountFile() async {
+    try {
+      var dir = await getApplicationDocumentsDirectory();
+      var file = await File(dir.path + '/count.txt').readAsString();
+      print(file);
+      setState(() {
+        _count = int.parse(file);
+      });
+    } catch (e) {
+      print(e.toString());
+    }
+  }
+}
