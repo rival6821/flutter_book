7aa781b108bba065ed7aff74bb87f791a3fb8d41
ilhoon <rival6821@naver.com>
2022-01-15 20:47:21 +0900

    add fruit

--- fileio_example/lib/fileApp.dart
+++ fileio_example/lib/fileApp.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:path_provider/path_provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class FileApp extends StatefulWidget {
   @override
@@ -10,10 +11,53 @@ class FileApp extends StatefulWidget {
 
 class _FileApp extends State<FileApp> {
   int _count = 0;
+  List<String> itemList = List.empty(growable: true);
+  TextEditingController controller = TextEditingController();
+
+  Future<List<String>> readListFile() async {
+    // List<String> itemList = List.empty(growable: true);
+    var key = 'first';
+    SharedPreferences pref = await SharedPreferences.getInstance();
+    bool? firstCheck = pref.getBool(key);
+    var dir = await getApplicationDocumentsDirectory();
+    bool fileExist = await File(dir.path + '/fruit.txt').exists();
+
+    if (firstCheck == null || firstCheck == false || fileExist == false) {
+      pref.setBool(key, true);
+      var file =
+          await DefaultAssetBundle.of(context).loadString('repo/fruit.txt');
+
+      File(dir.path + '/fruit.txt').writeAsStringSync(file);
+
+      var array = file.split('\n');
+      for (var item in array) {
+        print(item);
+        itemList.add(item);
+      }
+      return itemList;
+    } else {
+      var file = await File(dir.path + '/fruit.txt').readAsString();
+      var array = file.split('\n');
+      for (var item in array) {
+        print(item);
+        itemList.add(item);
+      }
+      return itemList;
+    }
+  }
+
   @override
   void initState() {
     super.initState();
     readCountFile();
+    initData();
+  }
+
+  void initData() async {
+    var result = await readListFile();
+    setState(() {
+      itemList.addAll(result);
+    });
   }
 
   @override
@@ -24,22 +68,47 @@ class _FileApp extends State<FileApp> {
       ),
       body: Container(
         child: Center(
-          child: Text(
-            '$_count',
-            style: TextStyle(fontSize: 40),
+          child: Column(
+            children: [
+              TextField(
+                controller: controller,
+                keyboardType: TextInputType.text,
+              ),
+              Expanded(
+                  child: ListView.builder(
+                itemBuilder: (context, index) {
+                  return Card(
+                    child: Center(
+                      child: Text(
+                        itemList[index],
+                        style: TextStyle(fontSize: 30),
+                      ),
+                    ),
+                  );
+                },
+                itemCount: itemList.length,
+              ))
+            ],
           ),
         ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
+          writeFruit(controller.value.text);
           setState(() {
-            _count++;
+            itemList.add(controller.value.text);
           });
-          writeCountFile(_count);
         },
         child: Icon(Icons.add),
       ),
     );
+  }
+
+  void writeFruit(String fruit) async {
+    var dir = await getApplicationDocumentsDirectory();
+    var file = await File(dir.path + '/fruit.txt').readAsString();
+    file = file + '\n' + fruit;
+    File(dir.path + '/fruit.txt').writeAsStringSync(file);
   }
 
   void writeCountFile(int count) async {
