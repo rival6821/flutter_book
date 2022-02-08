9c68d0b59f805cf4a636eb0aba9173cf535716cc
ilhoon <rival6821@naver.com>
2022-02-08 22:14:55 +0900

    animation

--- animation_example/lib/secondPage.dart
+++ animation_example/lib/secondPage.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 class SecondPage extends StatefulWidget {
@@ -7,7 +9,32 @@ class SecondPage extends StatefulWidget {
   _SecondPage createState() => _SecondPage();
 }
 
-class _SecondPage extends State<SecondPage> {
+class _SecondPage extends State<SecondPage>
+    with SingleTickerProviderStateMixin {
+  AnimationController? _animationController;
+  Animation? _rotateAnimation;
+  Animation? _scaleAnimation;
+  Animation? _transAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController =
+        AnimationController(vsync: this, duration: Duration(seconds: 5));
+    _rotateAnimation =
+        Tween<double>(begin: 0, end: pi * 10).animate(_animationController!);
+    _scaleAnimation =
+        Tween<double>(begin: 1, end: 0).animate(_animationController!);
+    _transAnimation = Tween<Offset>(begin: Offset(0, 0), end: Offset(200, 200))
+        .animate(_animationController!);
+  }
+
+  @override
+  void dispose() {
+    _animationController?.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,12 +43,33 @@ class _SecondPage extends State<SecondPage> {
         child: Center(
             child: Column(
           children: [
-            Hero(
+            AnimatedBuilder(
+              animation: _rotateAnimation!,
+              builder: (context, widget) {
+                return Transform.translate(
+                  offset: _transAnimation!.value,
+                  child: Transform.rotate(
+                    angle: _rotateAnimation!.value,
+                    child: Transform.scale(
+                      scale: _scaleAnimation!.value,
+                      child: widget,
+                    ),
+                  ),
+                );
+              },
+              child: Hero(
                 tag: 'detail',
                 child: Icon(
                   Icons.cake,
-                  size: 150,
-                )),
+                  size: 300,
+                ),
+              ),
+            ),
+            ElevatedButton(
+                onPressed: () {
+                  _animationController!.forward();
+                },
+                child: Text('로테이션 시작하기'))
           ],
           mainAxisAlignment: MainAxisAlignment.center,
         )),
