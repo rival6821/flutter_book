1aab0feacbec2d5061497aca226ef2f2f3e11aa4
ilhoon <rival6821@naver.com>
2022-02-12 22:46:54 +0900

    sliver header

--- animation_example/lib/sliverPage.dart
+++ animation_example/lib/sliverPage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math' as math;
 
 class SliverPage extends StatefulWidget {
   const SliverPage({Key? key}) : super(key: key);
@@ -54,5 +55,36 @@ class _SliverPageState extends State<SliverPage> {
         ),
       ),
     );
+  }
+}
+
+class _HeaderDelegate extends SliverPersistentHeaderDelegate {
+  final double minHeight;
+  final double maxHeight;
+  final Widget child;
+
+  _HeaderDelegate({
+    required this.minHeight,
+    required this.maxHeight,
+    required this.child,
+  });
+
+  @override
+  Widget build(
+      BuildContext context, double shrinkOffset, bool overlapsContent) {
+    return SizedBox.expand(child: child);
+  }
+
+  @override
+  double get maxExtent => math.max(maxHeight, minHeight);
+
+  @override
+  double get minExtent => minHeight;
+
+  @override
+  bool shouldRebuild(_HeaderDelegate oldDelegate) {
+    return maxHeight != oldDelegate.maxExtent ||
+        minHeight != oldDelegate.minHeight ||
+        child != oldDelegate.child;
   }
 }
