8d1187a0e7b67f3a0d8abf4167bd565ff51dfa7d
ilhoon <rival6821@naver.com>
2022-02-13 18:38:07 +0900

    get device info

---
+++ native_example/lib/main.dart
@@ -0,0 +1,82 @@
+import 'dart:io';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    if (Platform.isIOS) {
+      return CupertinoApp(home: CupertinoNativeApp());
+    } else {
+      return MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(primarySwatch: Colors.blue),
+        home: NativeApp(),
+      );
+    }
+  }
+}
+
+class CupertinoNativeApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _NativeApp();
+  }
+}
+
+class NativeApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _NativeApp();
+  }
+}
+
+class _NativeApp extends State<StatefulWidget> {
+  static const platform = const MethodChannel('com.flutter.dev/info');
+  String _deviceInfo = 'Unknown info';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Native 통신 예제'),
+      ),
+      body: Container(
+        child: Center(
+          child: Text(
+            _deviceInfo,
+            style: TextStyle(fontSize: 30),
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          _getDeviceInfo();
+        },
+        child: Icon(Icons.get_app),
+      ),
+    );
+  }
+
+  Future<void> _getDeviceInfo() async {
+    String deviceInfo;
+    try {
+      final String result = await platform.invokeMethod('getDeviceInfo');
+      deviceInfo = 'Device info : $result';
+    } on PlatformException catch (e) {
+      deviceInfo = 'Failed to get Device info: ${e.message}';
+    }
+    setState(() {
+      _deviceInfo = deviceInfo;
+    });
+  }
+}
